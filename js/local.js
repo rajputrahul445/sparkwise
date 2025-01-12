5c000d2f708674900ab0a4bc157a50528974c269
Rahul Bhardwaj <rajputrahul445@gmail.com>
2025-01-12 22:29:07 +0530

    Pricing page update

--- js/local.js
+++ js/local.js
@@ -67,3 +67,51 @@ $('#tab-3').click( function() {
     $('#w-tabs-0-data-w-pane-3').siblings().removeClass('w--tab-active');
     $('#w-tabs-0-data-w-pane-3').addClass('w--tab-active');
 });
+
+
+
+$('#custom-tab-1').click( function() {
+    $('#tab-content-1').siblings().removeClass('active');
+    $('#tab-content-1').addClass('active');
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+});
+$('#custom-tab-2').click( function() {
+    $('#tab-content-2').siblings().removeClass('active');
+    $('#tab-content-2').addClass('active');
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+});
+$('#custom-tab-3').click( function() {
+    $('#tab-content-3').siblings().removeClass('active');
+    $('#tab-content-3').addClass('active');
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+});
+
+
+$(function() {
+      // initialize counter
+        var counter = 0;
+
+        // set the + functionality
+        $('#plus').click( function(){$('#display').html( ++counter )} );
+        // set the - functionality
+        $('#minus').click( function(){$('#display').html( (counter-1<0)?counter:--counter )} );
+        // initialize display
+        $('#display').html( counter );
+  });
+
+
+
+$(".addmore").click(function() {
+        var newRow = '<div class="addFields added-row">' + '<div class="form-group">' + '<div class="inputBx mb-1">' + '<select name="role[]" class="form-control" required>' + '<option value="" selected disabled>Role</option>' + '<option value="Software Developer/Engineer">Software Developer/Engineer</option>' + '<option value="Front-End Developer">Front-End Developer</option>' + '<option value="Back-End Developer">Back-End Developer</option>' + '<option value="Full-Stack Developer">Full-Stack Developer</option>' + '<option value="Mobile Developer">Mobile Developer</option>' + '<option value="Game Developer">Game Developer</option>' + '<option value="DevOps Engineer">DevOps Engineer</option>' + '<option value="Blockchain Developer">Blockchain Developer</option>' + '<option value="Systems Developer">Systems Developer</option>' + '<option value="QA Developer">QA Developer</option>' + '<option value="Data Scientist">Data Scientist</option>' + '<option value="Data Engineer">Data Engineer</option>' + '<option value="Data Analyst">Data Analyst</option>' + '<option value="AI/ML Engineer">AI/ML Engineer</option>' + '<option value="Business Intelligence (BI) Developer">Business Intelligence (BI) Developer</option>' + '<option value="Data Security Analyst">Data Security Analyst</option>' + '<option value="Statistician">Statistician</option>' + '<option value="UI/UX Designer">UI/UX Designer</option>' + '<option value="Graphic Designer">Graphic Designer</option>' + '<option value="Motion Graphics Designer">Motion Graphics Designer</option>' + '<option value="Industrial Designer">Industrial Designer</option>' + '<option value="3D Artist">3D Artist</option>' + '<option value="Project Manager">Project Manager</option>' + '<option value="Product Manager">Product Manager</option>' + '<option value="Accountant">Accountant</option>' + '</select>' + '</div>' + '</div>' + '<div class="form-group">' + '<div class="inputBx mb-1">' + '<select name="experience[]" class="form-control" required>' + '<option value="" selected disabled>Year of Experience</option>' + '<option value="Fresher (0-1 Years)">Fresher (0-1 Years)</option>' + '<option value="Mid Level (2-4 Years)">Mid Level (2-4 Years)</option>' + '<option value="Senior (More than 5 Years)">Senior (More than 5 Years)</option>' + '</select>' + '</div>' + '</div>' + '<div class="qtyCLsWrp">' +  '<div class="qtyCLs">' + '<span id="minus">-</span>' + '<div id="display">0</div>' + '<span id="plus">+</span>'  + '</div>' +  '</div>' + '<div class="fullWidth">' + '<button class="removeBtn"><img src="images/cross.png"/></button>' + ' </div>' + ' </div>';
+        $(".addmore").before(newRow);
+    });
+    // Remove Button
+    $(document).on('click', '.removeBtn', function() {
+        $(this).closest('.added-row').remove();
+    });
+
+
+
